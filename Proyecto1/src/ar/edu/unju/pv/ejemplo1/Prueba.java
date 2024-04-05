ef754e1bb4051e792da1feffe4b17c79478d13a3
PC <PC@DESKTOP-HS9BH74>
2024-04-05 20:53:32 -0300

    SUBO PRIMER COMMIT DEL PROYECTO

---
+++ Proyecto1/src/ar/edu/unju/pv/ejemplo1/Prueba.java
@@ -0,0 +1,35 @@
+package ar.edu.unju.pv.ejemplo1;
+
+import java.util.Scanner;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		int anio = 2028;
+		String saludo = "Hola Pv";
+		System.out.println(saludo + " " + anio);
+
+		if (anio <= 2024) {
+			System.out.println("Año correcto");
+		} else {
+			System.out.println("El año es incorrecto");
+		}
+
+		int tamanio = saludo.length();
+		for (int i = 0; i < tamanio; i++) {
+			System.out.println(saludo.charAt(i));
+		}
+		int indice = 0;
+		while (indice < tamanio) {
+			System.out.println(saludo.charAt(indice));
+			indice++;
+		}
+
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("ingrese un nombre");
+		String nombre = teclado.next();
+		System.out.println("Hola " + nombre);
+
+	}
+
+}
